6622eeb2b6818e96a055219eb9946d507c3b9971
Dell <ola_paprocka@o2.pl>
2019-06-15 11:43:20 +0200

    init

---
+++ src/main/java/springcalculator/CalculatorMain.java
@@ -0,0 +1,10 @@
+package springcalculator;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        CalculatorService calculatorService = new CalculatorService();
+        System.out.println(calculatorService.calculate("2+2"));
+    }
+
+
+}
